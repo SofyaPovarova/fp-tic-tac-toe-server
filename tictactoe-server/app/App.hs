076e3f8be9bcecb4d0c4f0d3221fddc73d9c0b4b
SofyaPovarova <sofya.povarova@mail.ru>
2020-10-03 23:43:03 +0300

    added something with auth

--- tictactoe-server/app/App.hs
+++ tictactoe-server/app/App.hs
@@ -1,21 +1,22 @@
+{-# LANGUAGE DataKinds #-}
+
 module Main where
 
 import Servant
 import Network.Wai.Handler.Warp
 import Routes
-import Game
+import Servant.Auth.Server (defaultJWTSettings, defaultCookieSettings, generateKey, JWT)
 
 main :: IO ()
 main = do
   let port = 8081
+
+  myKey <- generateKey
+  let jwtCfg = defaultJWTSettings myKey
+  let cfg = defaultCookieSettings :. jwtCfg :. EmptyContext
+      
   putStrLn $ "========\nStarting server on localhost:" ++ show port
-  run port app
-
-app :: Application
-app = serve gameAPI server
-
-gameAPI :: Proxy GameAPI
-gameAPI = Proxy
-
-server :: Server GameAPI
-server = return (emptyField 3)
+  run port $ serveWithContext api cfg (server defaultCookieSettings jwtCfg)
+  
+api :: Proxy (API '[JWT])
+api = Proxy
--- tictactoe-server/app/Routes.hs
+++ tictactoe-server/app/Routes.hs
@@ -1,9 +1,76 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds, DeriveGeneric, TypeOperators #-}
 
 module Routes where
 
+import Data.Aeson
+import Game
+import GHC.Generics (Generic)
 import Models
 import Servant
+import Servant.Auth.Server
+import Control.Monad.IO.Class (liftIO)
 
-type GameAPI = "field" :> Get '[JSON] Field
+type CookieHeaders = 
+  Headers '[ Header "Set-Cookie" SetCookie
+           , Header "Set-Cookie" SetCookie]
+             NoContent
+                                
+data User = User { name :: String, email :: String }
+   deriving (Eq, Show, Read, Generic)
+
+instance ToJSON User
+instance ToJWT User
+instance FromJSON User
+instance FromJWT User
+
+data Login = Login { username :: String, password :: String }
+   deriving (Eq, Show, Read, Generic)
+
+instance ToJSON Login
+instance FromJSON Login
+
+type Protected =
+  "field" :> Get '[JSON] Field
+  :<|> "user" :> Get '[JSON] String
+
+type Unprotected =
+  "register"
+     :> ReqBody '[JSON] Login
+     :> Post '[JSON] CookieHeaders
+  :<|> "login" 
+     :> ReqBody '[JSON] Login 
+     :> Post '[JSON] CookieHeaders
+     
+type API auths = (Servant.Auth.Server.Auth auths User :> Protected) :<|> Unprotected
+
+server :: CookieSettings -> JWTSettings -> Server (API auths)
+server cookieSettings jwtSettings =
+  protected :<|> unprotected cookieSettings jwtSettings
+
+
+protected :: Servant.Auth.Server.AuthResult User -> Server Protected
+protected (Servant.Auth.Server.Authenticated user) =
+  fieldHandler :<|> userHandler user
+protected _ = throwAll err401
+
+unprotected :: CookieSettings -> JWTSettings -> Server Unprotected
+unprotected cookieSettings jwtSettings = 
+  registrationHandler :<|> (loginHandler cookieSettings jwtSettings)
+  
+
+fieldHandler :: Handler Field
+fieldHandler = return (emptyField 3)
+
+userHandler :: User -> Handler String
+userHandler = return . show
+
+registrationHandler :: Login -> Handler CookieHeaders
+registrationHandler _ = undefined
+
+loginHandler :: CookieSettings -> JWTSettings -> Login -> Handler CookieHeaders
+loginHandler cookieSettings jwtSettings _ = do
+   let usr = User "Ali Baba" "ali@email.com"
+   mApplyCookies <- liftIO $ acceptLogin cookieSettings jwtSettings usr
+   case mApplyCookies of
+     Nothing           -> throwError err401
+     Just applyCookies -> return $ applyCookies NoContent
